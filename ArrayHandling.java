1732e7bad3b16610759c77ece4f6e65f80c201b5
VickySingh1998 <vickysingh8697@gmail.com>
2022-06-07 14:08:27 +0530

    modified: to find even position array element

--- ArrayHandling.java
+++ ArrayHandling.java
@@ -1,12 +1,10 @@
 package com.bridgelabz;
 public class ArrayHandling {
     public static void main(String[] args) {
-        int [] arr = new int[] {34, 6, 98, 76, 86, 5, 39};
-        int small = arr[0];
+        int [] arr = new int[] {34, 6, 98, 76, 86, 5};
+        System.out.println("The elements of an array present on an even position :- ");
 
-        for (int i : arr)
-            if (i < small)
-                small = i;
-        System.out.println("The largest element in array is "+small);
+        for (int i=1; i<arr.length; i=i+2)
+        System.out.println(arr[i]);
     }
 }
