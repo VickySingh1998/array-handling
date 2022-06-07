a73799566b9059ba517e3de6e22a2b008322b5da
VickySingh1998 <vickysingh8697@gmail.com>
2022-06-07 13:58:59 +0530

    modified: to find largest number

--- ArrayHandling.java
+++ ArrayHandling.java
@@ -1,26 +1,12 @@
 package com.bridgelabz;
 public class ArrayHandling {
     public static void main(String[] args) {
-        int [] arr = new int [] {1, 2, 3, 2, 4, 5, 5, 6};
-        int [] frequency = new int [arr.length];
-        int v = -1;
-        for(int i = 0; i < arr.length; i++){
-            int count = 1;
-            for(int j = i+1; j < arr.length; j++){
-                if(arr[i] == arr[j]){
-                    count++;
+        int [] arr = new int[] {34, 6, 98, 76, 86, 5, 39};
+        int large = arr[0];
 
-                    frequency[j] = v;
-                }
-            }
-            if(frequency[i] != v)
-                frequency[i] = count;
-        }
-        System.out.println(" Element | Frequency");
-        for(int i = 0; i < frequency.length; i++){
-            if(frequency[i] != v)
-                System.out.println("    " + arr[i] + "    |    " + frequency[i]);
-        }
-        System.out.println("----------------------------------------");
+        for (int i : arr)
+            if (i > large)
+                large = i;
+        System.out.println("The largest element in array is "+large);
     }
 }
