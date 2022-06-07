6d77f498b8861bc4f2a7e059afde18e8f9cabd1a
VickySingh1998 <vickysingh8697@gmail.com>
2022-06-07 14:01:25 +0530

    modified: to find smallest element

--- ArrayHandling.java
+++ ArrayHandling.java
@@ -2,11 +2,11 @@ package com.bridgelabz;
 public class ArrayHandling {
     public static void main(String[] args) {
         int [] arr = new int[] {34, 6, 98, 76, 86, 5, 39};
-        int large = arr[0];
+        int small = arr[0];
 
         for (int i : arr)
-            if (i > large)
-                large = i;
-        System.out.println("The largest element in array is "+large);
+            if (i < small)
+                small = i;
+        System.out.println("The largest element in array is "+small);
     }
 }
