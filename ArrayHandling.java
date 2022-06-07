7d7b030c4d43b26e57b0da3dfd41b3ce5d3f6cf9
VickySingh1998 <vickysingh8697@gmail.com>
2022-06-07 13:48:12 +0530

    modified: to find frequency of array elements

--- ArrayHandling.java
+++ ArrayHandling.java
@@ -1,10 +1,26 @@
 package com.bridgelabz;
-
 public class ArrayHandling {
     public static void main(String[] args) {
-        int roll[] = {1, 2, 3};
-        for (int i=0; i<3; i++) {
-            System.out.println("Roll No "+roll[i]);
+        int [] arr = new int [] {1, 2, 3, 2, 4, 5, 5, 6};
+        int [] frequency = new int [arr.length];
+        int v = -1;
+        for(int i = 0; i < arr.length; i++){
+            int count = 1;
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[i] == arr[j]){
+                    count++;
+
+                    frequency[j] = v;
+                }
+            }
+            if(frequency[i] != v)
+                frequency[i] = count;
         }
+        System.out.println(" Element | Frequency");
+        for(int i = 0; i < frequency.length; i++){
+            if(frequency[i] != v)
+                System.out.println("    " + arr[i] + "    |    " + frequency[i]);
+        }
+        System.out.println("----------------------------------------");
     }
 }
