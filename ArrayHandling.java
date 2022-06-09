8e81df173028beea5bfc86d9a752ad1935d144da
VickySingh1998 <vickysingh8697@gmail.com>
2022-06-09 07:25:24 +0530

    modified: to find odd position array element

--- ArrayHandling.java
+++ ArrayHandling.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 public class ArrayHandling {
     public static void main(String[] args) {
         int [] arr = new int[] {34, 6, 98, 76, 86, 5};
-        System.out.println("The elements of an array present on an even position :- ");
+        System.out.println("The elements of an array present on odd position :- ");
 
         for (int i=0; i<arr.length; i=i+2)
         System.out.println(arr[i]);
